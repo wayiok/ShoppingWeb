f8f31367b3abd8a6c5b390c4c7dbcb902be62e94
Cesar Añasco <cesar.anasco@hotmail.com>
2022-01-26 20:38:10 -0500

    Components Established
    Models and HTML files estructures added

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+
+
+  recipes: Recipe[] = [
+    new Recipe('pizza','test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'),
+    new Recipe('pizza','test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
