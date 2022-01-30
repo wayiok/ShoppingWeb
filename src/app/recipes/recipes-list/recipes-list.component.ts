22e4832d39b32263fd2aba443ef5ebf265ca2783
Cesar Añasco <cesar.anasco@hotmail.com>
2022-01-29 23:43:02 -0500

    Databinding
    Navigation and databinding between components established

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -8,14 +8,18 @@ import {Recipe} from "../recipe.model";
 })
 export class RecipesListComponent implements OnInit {
 
+  @Output() recipeOnListSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('pizza','test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'),
+    new Recipe('piasa','test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'),
     new Recipe('pizza','test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272')
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeOnListSelected.emit(recipe);
   }
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInputReference: ElementRef;
+  @ViewChild('amountInput') amountInputReference: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddItem() {
+    const  ingName = this.nameInputReference.nativeElement.value;
+    const  ingAmount = this.amountInputReference.nativeElement.value;
+    const  newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
